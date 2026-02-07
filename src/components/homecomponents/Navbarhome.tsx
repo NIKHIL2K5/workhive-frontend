cbf9994b28b5cf2848dc7e67a87f98c67e3e2fd8
Nikhil <241758358+NIKHILEGLOGICS@users.noreply.github.com>
2026-02-07 23:07:13 +0530

    Chnages done

--- src/components/homecomponents/Navbarhome.tsx
+++ src/components/homecomponents/Navbarhome.tsx
@@ -1,18 +1,35 @@
-function Navbar(){
-    return(
-        <>
-            <div>
-                <h1>Logo</h1>
-                <div>
-                    <a href="#">Explore Oppertunities</a>
-                    <a href="#">Find Freelancers</a>
-                </div>
-                <div>
-                    <button>Join the Hive</button>
-                    <button>Log in into Hive</button>
-                </div>
-            </div>
-        </>
-    )
+import { useNavigate } from "react-router-dom";
+
+function Navbar() {
+    const navigate = useNavigate();
+  return (
+    <nav className="w-full">
+      <div className="flex items-center justify-between w-full px-8 py-4">
+        <img
+          src="https://res.cloudinary.com/dboscnm7g/image/upload/v1763030368/WORKHIVE_LOGO-removebg-preview_pfnlpq.png"
+          className="h-20"
+          alt="WorkHive"
+          onClick={()=>navigate("/home")}
+        />
+
+        <div className="flex gap-10">
+          <p  className="font-medium font-[arial] font-bold">Explore Opportunities</p>
+          <p className="font-medium font-[arial] font-bold">Find Freelancers</p>
+        </div>
+
+        <div className="flex gap-4">
+          <button className="px-4 py-2 rounded-lg font-medium font-[arial] font-bold">
+            Join the Hive
+          </button>
+          <button className="px-4 py-2 rounded-lg font-medium font-[arial] font-bold">
+            Log in
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
+
+
+
 export default Navbar
